c5982f31a693b801f50fef2ae4eb368a95b7e88a
Thethela Faltein <fltthe004@sl-dual-127.cs.uct.ac.za>
2017-04-18 17:33:18 +0200

    I forgot to add BST here,and the testsuite from assignment1

---
+++ TestSuite.java
@@ -0,0 +1,21 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+TestBinaryTreeNode.class, TestPerson.class, TestBinarySearchTree.class
+
+
+
+
+
+
+
+})
+
+public class TestSuite{
+
+}
